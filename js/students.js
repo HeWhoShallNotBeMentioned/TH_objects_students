ce693e6e4826cbcb1d9160eb99fc0639c3cf4dbb
Chris Underwood <cunderwoodmn@gmail.com>
2016-09-28 20:52:43 -0700

    created students array object

---
+++ js/students.js
@@ -0,0 +1,32 @@
+var students = [
+  {
+    name: "a" ,
+    track: "a1",
+    achievements: 1,
+    points: 11
+  },
+  {
+    name: "b",
+    track: "b1",
+    achievements: 2,
+    points: 22
+  },
+  {
+    name: "c",
+    track: "c1",
+    achievements: 3,
+    points: 33
+  },
+  {
+    name: "d",
+    track: "d1",
+    achievements: 4,
+    points: 44
+  },
+  {
+    name: "e",
+    track: "e1",
+    achievements: 5,
+    points: 55
+  }
+];
